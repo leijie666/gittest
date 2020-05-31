c830d221a7e14a33ff0ab2ab2d9c58db97f8e4ee
LeiJie <3264142949@qq.com>
2020-05-31 20:05:02 +0800

    first commit

---
+++ src/com/hierway/entities/Student.java
@@ -0,0 +1,12 @@
+package com.hierway.entities;
+
+/**
+ *
+ * @description
+ * @date 2020/5/31 17:01
+ */
+public class Student {
+    private String name;
+
+
+}
---
+++ src/com/hierway/entities/User.java
@@ -0,0 +1,9 @@
+package com.hierway.entities;
+
+/**
+ * @author LeiJie
+ * @date 2020/5/31 18:44
+ */
+public class User {
+    private String name;
+}
